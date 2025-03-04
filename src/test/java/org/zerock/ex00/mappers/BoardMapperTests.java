71e8b1e7f4ce3a55fc07ebdfee6a2a5914b988c7
neoulnabom <qkfkwlak12@gmail.com>
2025-03-04 09:27:39 +0900

    List commit

--- src/test/java/org/zerock/ex00/mappers/BoardMapperTests.java
+++ src/test/java/org/zerock/ex00/mappers/BoardMapperTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.ex00.domain.BoardVO;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -23,5 +24,37 @@ public class BoardMapperTests {
     @Test
     public void testList() {
         boardMapper.getList().forEach(boardVO -> log.info(boardVO));
+    }
+
+    @Test
+    public void testInsert() {
+
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("NewTest");
+        boardVO.setContent("New Test...");
+        boardVO.setWriter("newbie");
+
+        log.info("COUNT: " + boardMapper.insert(boardVO));
+
+        log.info("BNO: " + boardVO.getBno());
+    }
+
+    @Test
+    public void testSelect(){
+        Long bno = 9L;
+
+        log.info(boardMapper.select(bno));
+    }
+
+    @Test
+    public void testUpdate(){
+
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("Updated Title");
+        boardVO.setContent("Updated Content");
+        boardVO.setBno(9L);
+        int updateContent = boardMapper.update(boardVO);
+
+        log.info("update: " + updateContent);
     }
 }
